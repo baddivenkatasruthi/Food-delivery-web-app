f4e98afd9fa59f7cb7609e6b610bcda135a2d31a
baddivenkatasruthi <baddivenkatasruthi25@gmail.com>
2026-03-16 14:17:55 +0530

    Delete Food Delivery App/Backend/models/Restaurant.js

--- Food Delivery App/Backend/models/Restaurant.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const restaurantSchema = new mongoose.Schema({
-  name: String,
-  location: String,
-  isActive: Boolean
-});
-
-module.exports = mongoose.model("Restaurant", restaurantSchema);
